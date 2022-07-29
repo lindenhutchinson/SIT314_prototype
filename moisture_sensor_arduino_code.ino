e191aa684cb6ab942d9d2a112d2f9b682e87d003
Linden Hutchinson <hutchinson.linden@gmail.com>
2022-07-29 13:16:41 +1000

    add missing brace

--- moisture_sensor_arduino_code.ino
+++ moisture_sensor_arduino_code.ino
@@ -3,26 +3,30 @@
 #define sensorPin A0
 #define sixHoursInMilliseconds 21600000
 
-void setup() {
-   pinMode(sensorPower, OUTPUT);
-	
+void setup()
+{
+	pinMode(sensorPower, OUTPUT);
+
 	// Initially keep the sensor OFF
 	digitalWrite(sensorPower, LOW);
 	Serial.begin(9600);
 }
 
-void loop() {
-  	// only check the sensor every 6 hours
-  	if(millis() % sixHoursInMilliseconds == 0)
-      // output the probe resistance to serial console
-      Serial.println(readSensor());
+void loop()
+{
+	// only check the sensor every 6 hours
+	if (millis() % sixHoursInMilliseconds == 0)
+	{
+		// output the probe resistance to serial console
+		Serial.println(readSensor());
 	}
 }
 
-int readSensor() {
-	digitalWrite(sensorPower, HIGH);	// Turn the sensor ON
-	delay(10);							// Allow power to settle
-	int val = analogRead(sensorPin);	// Read the analog value form sensor
-	digitalWrite(sensorPower, LOW);		// Turn the sensor OFF
-	return val;							// Return analog moisture value
+int readSensor()
+{
+	digitalWrite(sensorPower, HIGH); // Turn the sensor ON
+	delay(10);						 // Allow power to settle
+	int val = analogRead(sensorPin); // Read the analog value form sensor
+	digitalWrite(sensorPower, LOW);	 // Turn the sensor OFF
+	return val;						 // Return analog moisture value
 }
